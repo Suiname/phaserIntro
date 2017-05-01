ddfc1a60ac8dcaf009817cf26d30ef68633e8c7c
Jason Tham <jtham@sbcglobal.net>
2017-04-30 22:23:44 -0500

    scaled chicken to twice size

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
-const game = new Phaser.Game(640, 360, Phaser.AUTO);
-
 const GameState = {
-    preload: () => {
-
+    preload() {
+        this.load.image('background', 'assets/images/background.png');
+        this.load.image('chicken', 'assets/images/chicken.png');
     },
-    create: () => {
-
+    create() {
+        this.background = this.game.add.sprite(0, 0, 'background');
+        this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
+        this.chicken.anchor.setTo(0.5, 0.5);
+        this.chicken.scale.setTo(2);
     },
-    update: () => {
+    update() {
 
     }
 };
+
+const game = new Phaser.Game(640, 360, Phaser.AUTO);
 
 game.state.add('GameState', GameState);
 game.state.start('GameState');
