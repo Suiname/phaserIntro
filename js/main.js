b479545e781ee6fc280ad82570ecc08a1e6656c2
Jason Tham <jtham@sbcglobal.net>
2017-04-30 22:33:32 -0500

    added alignment and scaling of screen

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ const GameState = {
         this.load.image('chicken', 'assets/images/chicken.png');
     },
     create() {
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
         this.background = this.game.add.sprite(0, 0, 'background');
         this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
         this.chicken.anchor.setTo(0.5, 0.5);
